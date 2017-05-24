2038d6c87cc60c4c5dea2350247ee3ff28b1bb40
ManbirJaspal <manb.jaspal@gmail.com>
2017-05-24 16:11:20 +0530

    added button, made them clickable using ng-click.

---
+++ todo/public/app.js
@@ -0,0 +1,45 @@
+/**
+ * Created by manbir on 5/18/2017.
+ */
+(function(){
+
+    angular
+        .module('TodoApp', [])
+    .controller('TodoController', TodoController);
+
+    function TodoController($scope, $http) {    //The scope is the binding part between the HTML (view) and the JavaScript (controller).|| The scope is an object with the available properties and methods.|| The scope is available for both the view and the controller. -->
+            $scope.todos = [];                  //$http allows us to talk to http resources, like our server.
+
+    $scope.updateTodo = function() {
+        $scope.todos[ $scope.selectedTodoIndex].name = $scope.name;
+    }
+            $scope.selectTodo = function(index) {
+                $scope.name = $scope.todos[index].name;
+                $scope.selectedTodoIndex = index;          //<!-- to remember the index we selected , so that we can use it to update the value -->
+            }
+            $scope.deleteTodo = function(index){
+                console.log('remove ' + index );
+                $scope.todos.splice(index, 1);  //<!-- The splice() method adds/removes items to/from an array, and returns the removed item(s), note that it changes the array   -->
+            }
+           $scope.newTodo = function() {
+            console.log('newTodo: ' + $scope.name);
+
+
+
+            $http.get('/todo/' + $scope.name)
+                .success(function(todos) {
+                  $scope.todos = todos;
+                })
+
+
+
+
+
+     }
+
+
+
+    }
+
+
+})();
--- todo/server.js
+++ todo/server.js
@@ -3,3 +3,26 @@
  */
 var express = require('express');
 var app = express();
+
+
+app.get('/hello', handleRootRequest);
+app.get('/todo/:name', createTodo);
+
+  var todos = [];
+function createTodo(req, res) {
+    var name = req.params['name'];
+    var todo = {
+        name:name
+    };
+
+    todos.push(todo); //pushes object to the array
+    res.send(todos);
+}
+
+
+app.use(express.static(__dirname + '/public'));
+function handleRootRequest(req, res){
+    res.send('hello world');
+}
+
+app.listen(3000);
